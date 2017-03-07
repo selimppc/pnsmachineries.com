b260005305bcb5022777566f845b74ecc59d7212
mithunadhikary <mithun.cse521@gmail.com>
2017-03-07 09:25:28 -0700

    mail template check 1

--- wp-content/themes/pnsmachine/single-mailtemplate.php
+++ wp-content/themes/pnsmachine/single-mailtemplate.php
@@ -1,5 +1,22 @@
 <?php
+$to = "mithun.cse521@gmail.com";
 
+			$message = "aaaaa";
+
+			$subject = "HTML email";
+
+			$headers = "MIME-Version: 1.0" . "\r\n";
+			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+			// More headers
+			$headers .= 'From: <webmaster@example.com>' . "\r\n";
+			$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+			if(mail($to,$subject,$message,$headers)){
+				echo 'Mail Send';
+			}else{
+				echo 'Not Send';
+			}
 	if ( have_posts() ) :
 		while ( have_posts() ) : the_post();
 
@@ -127,7 +144,11 @@
 			$headers .= 'From: <webmaster@example.com>' . "\r\n";
 			$headers .= 'Cc: myboss@example.com' . "\r\n";
 
-			mail($to,$subject,$message,$headers);
+			if(mail($to,$subject,$message,$headers)){
+				echo 'Mail Send';
+			}else{
+				echo 'Not Send';
+			}
 
 		endwhile;
 	endif;
