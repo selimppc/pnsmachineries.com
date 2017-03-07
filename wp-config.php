23624afda3c7418cb1d772906816aea3f07a3f94
mithunadhikary <mithun.cse521@gmail.com>
2017-03-07 01:08:17 -0700

    Add wp-confif

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@ define('WP_CACHE', true); // Added by WP Rocket
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'pns');
+define('DB_NAME', 'pnsmachineries');
 
 /** MySQL database username */
 define('DB_USER', 'root'); 
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
