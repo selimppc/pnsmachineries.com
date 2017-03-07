fb5cbe4ef2f0e1f939d3a124b6381b4386203abd
mithunadhikary <mithun.cse521@gmail.com>
2017-03-07 09:06:54 -0700

    mail template check 1

--- wp-content/themes/pnsmachine/single-mailtemplate.php
+++ wp-content/themes/pnsmachine/single-mailtemplate.php
@@ -3,9 +3,17 @@
 	if ( have_posts() ) :
 		while ( have_posts() ) : the_post();
 
-			echo 'hello';
+			$banner_image_id = get_post_meta($post->ID,'banner-photo', true);
+			$banner_image = wp_get_attachment_image_src($banner_image_id,'full');
+			
+			$basic_fields = get_post_meta($post->ID,'basic-fields', true);
+			$product_data_r = get_post_meta($post->ID,'mail-template-product', true);
 
+			$first_block = array_slice($product_data_r,0,3);
+			$second_block = array_slice($product_data_r,3,3);
+			$third_block = array_slice($product_data_r,6,3);
 
+			echo '2';
 
 		endwhile;
 	endif;
