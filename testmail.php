a3bfac4fb3c71c1495ef652924f84d0f08cb7899
mithunadhikary <mithun.cse521@gmail.com>
2017-03-08 01:29:20 -0700

    create testmail.php for only mail function

---
+++ testmail.php
@@ -0,0 +1,17 @@
+<?php
+	$to = "mithun.cse521@gmail.com";
+	$subject = "HTML email";
+	// Always set content-type when sending HTML email
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+	// More headers
+	$headers .= 'From: <webmaster@example.com>' . "\r\n";
+	$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+	if(mail($to,$subject,'Test mail',$headers)){
+		echo 'Send';
+	}else{
+		echo 'not send';
+	}
+?>
